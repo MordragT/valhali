0baab0912522e4e2132a94ecec6e52b060915284
Thomas Wehmöller <connect.mordrag@gmx.de>
2024-05-13 13:30:19 +0200

    🎉 First rc for 0.1.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
         toolchain = pkgs.rustPlatform;
-      in rec {
+      in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             (with toolchain; [
@@ -33,7 +33,7 @@
           RUST_SRC_PATH = "${toolchain.rustLibSrc}";
         };
 
-        packages.default = packages.valhali;
+        packages.default = self.packages.valhali;
         packages.valhalid = toolchain.buildRustPackage {
           pname = "valhalid";
           version = "0.1.0";
@@ -48,69 +48,73 @@
         };
 
         apps.default = utils.lib.mkApp {
-          drv = packages.default;
-        };
-
-        nixosModules.default = {
-          config,
-          lib,
-          ...
-        }: let
-          cfg = config.services.valhali;
-        in {
-          options.services.valhali = {
-            enable = lib.mkEnableOption "Valhali";
-            services = lib.mkOption {
-              description = "Valhali service definitions";
-              default = {};
-              type = lib.types.attrsOf (lib.types.submodule ({...}: {
-                options = {
-                  alias = lib.mkOption {
-                    description = "Optional alias semantically associated with the service";
-                    type = lib.types.nullOr lib.types.nonEmptyStr;
-                    default = null;
-                  };
-
-                  kind = lib.mkOption {
-                    description = "Service kind e.g. http";
-                    type = lib.types.nonEmptyStr;
-                  };
-
-                  protocol = lib.mkOption {
-                    description = "Underlying transport protocol";
-                    type = lib.types.enum ["tcp" "udp"];
-                    default = "tcp";
-                  };
-
-                  port = lib.mkOption {
-                    description = "The port on which to advertise the service";
-                    type = lib.types.port;
-                  };
-                };
-              }));
-            };
-            aliases = lib.mkOption {
-              description = "Valhali alias definitions";
-              default = [];
-              type = lib.types.listOf lib.types.nonEmptyStr;
-            };
-          };
-
-          config = lib.mkIf cfg.enable {
-            environment.etc."valhali/config.toml".source = (pkgs.formats.toml {}).generate "config.toml" {
-              inherit (cfg) aliases services;
-            };
-
-            systemd.services.valhali = {
-              description = "Valhali daemon";
-              wantedBy = ["multi-user.target"];
-
-              serviceConfig = {
-                ExecStart = "${packages.valhalid}/bin/valhalid /etc/valhali/config.toml";
-              };
-            };
-          };
+          drv = self.packages.default;
         };
       }
-    );
+    )
+    // {
+      nixosModules.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        cfg = config.services.valhali;
+      in {
+        options.services.valhali = {
+          enable = lib.mkEnableOption "Valhali";
+          services = lib.mkOption {
+            description = "Valhali service definitions";
+            default = {};
+            type = lib.types.attrsOf (lib.types.submodule ({...}: {
+              options = {
+                alias = lib.mkOption {
+                  description = "Optional alias semantically associated with the service";
+                  type = lib.types.nullOr lib.types.nonEmptyStr;
+                  default = null;
+                };
+
+                kind = lib.mkOption {
+                  description = "Service kind e.g. http";
+                  type = lib.types.nonEmptyStr;
+                };
+
+                protocol = lib.mkOption {
+                  description = "Underlying transport protocol";
+                  type = lib.types.enum ["tcp" "udp"];
+                  default = "tcp";
+                };
+
+                port = lib.mkOption {
+                  description = "The port on which to advertise the service";
+                  type = lib.types.port;
+                };
+              };
+            }));
+          };
+          aliases = lib.mkOption {
+            description = "Valhali alias definitions";
+            default = [];
+            type = lib.types.listOf lib.types.nonEmptyStr;
+          };
+        };
+
+        config = lib.mkIf cfg.enable {
+          environment.etc."valhali/config.toml".source = (pkgs.formats.toml {}).generate "config.toml" {
+            inherit (cfg) aliases services;
+          };
+
+          services.avahi.enable = true;
+
+          systemd.services.valhali = {
+            description = "Valhali daemon";
+            wantedBy = ["multi-user.target"];
+
+            serviceConfig = {
+              ExecStart = "${self.packages.${pkgs.system}.valhalid}/bin/valhalid /etc/valhali/config.toml";
+            };
+          };
+        };
+      };
+    };
 }
